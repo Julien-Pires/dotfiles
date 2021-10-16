63bff43820eaac0c0eb4b1012aaac0cfc88cf464
Julien Pires <jpires@dashlane.com>
2021-10-16 17:13:24 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -1,3 +1,2 @@
 # Install chezmoi
-curl -sfL https://git.io/chezmoi | sh -s -- -b /usr/local/bin
-chezmoi init --apply Julien-Pires
+$ sh -c "$(curl -fsLS git.io/chezmoi)" -- init --apply Julien-Pires
