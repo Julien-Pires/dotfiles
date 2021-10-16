ff5fda7e560ac24b402f831e4c40c05bc91fbb4d
Julien Pires <jpires@dashlane.com>
2021-10-16 17:15:23 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
+# Install Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+
 # Install chezmoi
 sh -c "$(curl -fsLS git.io/chezmoi)" -- init --apply Julien-Pires
