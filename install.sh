94197ba51f8d1800e0db36a94a7badaaa1522b63
Julien Pires <jpires@dashlane.com>
2021-10-18 23:43:46 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
+# Install brew for Mac
+if [[ uname == 'Darwin' ]]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Install FiraCode font
+if [[ uname == 'Darwin' ]]; then
+    brew tap homebrew/cask-fonts
+    brew install --cask font-fira-code
+else
+    sudo apt install fonts-firacode
+fi
+
 # Install Powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
